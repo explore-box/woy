ebe5d2882a1c56f20fde4aa72019fd7f85bd2d6e
nyomansunima <nyomansunima@gmail.com>
2023-04-18 16:22:38 +0800

    :fire: feat: update backend and funding

---
+++ backend/src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
+import * as compression from 'compression'
+import helmet from 'helmet'
+
+const PORT = parseInt(process.env.PORT, 10) || 4000
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  // complete app settings
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+  app.use(compression())
+  app.use(helmet())
+  app.enableCors({ origin: '*' })
+
+  await app.listen(PORT, () => {
+    console.log(`Application running at http://localhost:${PORT}`)
+  })
+}
+bootstrap()
