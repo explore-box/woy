0c097a05e3372ef392853bb3751b4649459c04da
nyomansunima <nyomansunima@gmail.com>
2023-04-20 09:32:21 +0800

    🏓 chore: add tigris module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { ThrottlerModule } from '@nestjs/throttler'
+import { TigrisModule } from './tigris/tigris.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     ThrottlerModule.forRoot({ limit: 10, ttl: 60 }),
+    TigrisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
